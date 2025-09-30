51cc18de7db8f32ed94a20232ab58358650b2a09
Alexis de Lattre <alexis.delattre@akretion.com>
2025-09-30 11:55:16 +0200

    [FIX] account_statement_import_in_invoice: chatter message
    Only display the message in the chatter that we have forced the VAT
    amount only when it really happened.

--- account_statement_import_in_invoice/models/account_bank_statement_line.py
+++ account_statement_import_in_invoice/models/account_bank_statement_line.py
@@ -234,13 +234,14 @@ class AccountBankStatementLine(models.Model):
                     "This should never happen."
                 )
             )
-        invoice.message_post(
-            body=_(
-                "Total VAT Amount has been forced from %(ini_vat_amount)s to %(vat_amount)s.",
-                ini_vat_amount=format_amount(self.env, ini_vat_amount, cur),
-                vat_amount=format_amount(self.env, invoice.amount_tax, cur),
+        if invoice.currency_id.compare_amounts(ini_vat_amount, invoice.amount_tax):
+            invoice.message_post(
+                body=_(
+                    "Total VAT Amount has been forced from %(ini_vat_amount)s to %(vat_amount)s.",
+                    ini_vat_amount=format_amount(self.env, ini_vat_amount, cur),
+                    vat_amount=format_amount(self.env, invoice.amount_tax, cur),
+                )
             )
-        )
 
     def _prepare_in_invoice(self):
         self.ensure_one()
